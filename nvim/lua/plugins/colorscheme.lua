e28e671d9afced3c59d37eaf08834a98cc25931b
devgustavoa <dev.gustavoa@gmail.com>
2026-03-05 06:56:05 -0300

    chore: changed colorscheme to kanagawa wave

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,37 @@
 return {
-  {
-    "ficcdaf/ashen.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = {
-        bold = false,
-        italic = false,
-      },
-      transparent = true,
-    },
-    config = function(_, opts)
-      require("ashen").setup(opts)
-      vim.cmd.colorscheme("ashen")
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require("kanagawa").setup({
+            theme = "wave",
+            transparent = true,
+            background = {
+                dark = "wave",
+            },
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none",
+                        },
+                    },
+                },
+            },
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+                    Pmenu = { fg = theme.ui.shade0, bg = "none" },
+                    PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                }
+            end,
+        })
+        vim.cmd.colorscheme("kanagawa")
     end,
-  },
 }
