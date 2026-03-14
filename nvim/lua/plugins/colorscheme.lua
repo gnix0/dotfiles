10d4f02018b2f16e9592019f1b65719f1037a33d
Gustavo Arantes <dev.gustavoa@gmail.com>
2026-03-14 10:26:15 -0300

    udpate colorscheme to rose-pine

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,13 @@
 return {
-    "rebelot/kanagawa.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-        require("kanagawa").setup({
-            theme = "wave",
-            transparent = true,
-            background = {
-                dark = "wave",
+        require("rose-pine").setup({
+            variant = "main",
+            styles = {
+                transparency = true,
             },
-            colors = {
-                theme = {
-                    all = {
-                        ui = {
-                            bg_gutter = "none",
-                        },
-                    },
-                },
-            },
-            overrides = function(colors)
-                local theme = colors.theme
-                return {
-                    NormalFloat = { bg = "none" },
-                    FloatBorder = { bg = "none" },
-                    FloatTitle = { bg = "none" },
-                    Pmenu = { fg = theme.ui.shade0, bg = "none" },
-                    PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
-                    PmenuSbar = { bg = theme.ui.bg_m1 },
-                    PmenuThumb = { bg = theme.ui.bg_p2 },
-                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                }
-            end,
         })
-        vim.cmd.colorscheme("kanagawa")
+        vim.cmd.colorscheme("rose-pine")
     end,
 }
