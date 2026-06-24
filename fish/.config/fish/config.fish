515d6b3560b2d86ed439d1f2431cb5ec9667436a
Gustavo Arantes <dev.gustavoa@gmail.com>
2026-06-24 20:58:45 -0300

    emacs: remove previous configurations (will make my own from scratch)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fish_add_path ~/.config/emacs/bin
-    set -gx EMACSDIR ~/.config/emacs
 end
 
 set -U fish_greeting ""
