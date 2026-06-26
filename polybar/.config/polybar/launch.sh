b490124e13a895808466250f7de9b38ed66d7b37
Gustavo Arantes <dev.gustavoa@gmail.com>
2026-06-26 18:08:36 -0300

    polybar/wpp: show polybar on both screens, changed wallpaper

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# Terminate already running bar instances
+
 pkill polybar
-# Wait until the processes have been shut down
+
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-# Launch the bar
-polybar example &
+
+polybar --reload laptop &
+polybar --reload monitor &
